1e29a232e83c2857126813e064d527084eba290c
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-02-26 15:38:54 +0100

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,15 @@
+const buttonRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+buttonRef.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanRef.textContent = color;
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
